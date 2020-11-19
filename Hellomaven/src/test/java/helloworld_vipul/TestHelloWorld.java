da01263fb75385a07f5e16011ec863a88d84f922
vipul <vipulkshirsagar22@gmail.com>
2020-11-19 20:27:51 +0530

    commint neww

---
+++ Hellomaven/src/test/java/helloworld_vipul/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld_vipul;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+    @Test
+	public static void main(String[] args) {
+		System.out.println("FirstMaven..");
+		
+
+	}
+
+}
